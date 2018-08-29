981162cc558ec4960e65dc7edfbbcee520c2e1af
Campbell Miller <campbellmiller4e@gmail.com>
2018-08-29 16:14:22 +0100

    edits .listen

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
 app.use(express.static('public'));
 
 
-const server = app.listen(3000, function () {
+const server = app.listen(process.env.PORT || 3000, function () {
   const host = server.address().address;
   const port = server.address().port;
 
